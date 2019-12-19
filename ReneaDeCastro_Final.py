ae25d3aeeb2c0acaaff8d199798448c030114fbf
reneadc96 <reneadc96@gmail.com>
2019-12-19 15:19:47 -0800

    test

--- ReneaDeCastro_Final.py
+++ ReneaDeCastro_Final.py
@@ -1,9 +1,7 @@
 #Renea De Castro Final Assignment for CIS 303
 #REPL LINK: 
 # https://repl.it/@reneadc/ReneaDeCastroFinalCIS303Project
-#Making some code changes
-#here
-#and here
+
 import time;
 import subprocess; 
 #To clear screen on Windows/Linux/MacOSx Systems
